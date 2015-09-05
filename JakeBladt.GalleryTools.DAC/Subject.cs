fa37ab193929780db1e3dbfb7fe9243be355509a
Jake <jake@jakebladt.com>
2015-09-04 21:23:46 -0400

    basic props

--- JakeBladt.GalleryTools.DAC/Subject.cs
+++ JakeBladt.GalleryTools.DAC/Subject.cs
@@ -8,5 +8,10 @@ namespace JakeBladt.GalleryTools.DAC
 {
     public class Subject
     {
+        public virtual int Id { get; set; }
+        public virtual string FilePrefix { get; set; }
+        public virtual string DisplayName { get; set; }
+        public virtual int FileCount { get; set; }
+        public virtual DateTime AddedAt { get; set; }
     }
 }
