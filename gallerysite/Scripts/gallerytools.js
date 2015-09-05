8dbffd8353064db4f4e2d4a134cf1b4ba2aa9b8e
Jake <jake@jakebladt.com>
2015-09-04 20:34:34 -0400

    call API

---
+++ gallerysite/Scripts/gallerytools.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    console.log('page ready.')
+
+    $('#migrationStatusButton').click(function () {
+        $.get({
+            url: 'api/Migrate'
+        }).done(function(data) {
+            console.log(data);
+        });
+    });
+});
---
+++ gallerysite/api/ViewModels/MigrationStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gallerysite.api.ViewModels
+{
+    public class MigrationStatusViewModel
+    {
+        public int SubjectsInDatabase { get; set; }
+        public int SubjectsInFileSystem { get; set; }
+    }
+}
