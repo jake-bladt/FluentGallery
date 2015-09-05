6ce4c521bb783b4a8835eec6b3dd02ff305d6f43
Jake <jake@jakebladt.com>
2015-09-05 11:07:45 -0400

    update on click

--- gallerysite/Scripts/gallerytools.js
+++ gallerysite/Scripts/gallerytools.js
@@ -2,17 +2,16 @@
     migrationStatus: ko.observable({
         SubjectsInDatabase: 0,
         SubjectsInFileSystem: 0
-    })
+    }),
+
+    updateMigrationStatus: function (e) {
+        e.preventDefault();
+        $.get('/api/Migrate').done(function (data) {
+            this.migrationStatus(data);
+        });
+    }
 };
 
 $(document).ready(function () {
     ko.applyBindings(toolsViewModel);
-
-    $('#migrationStatusButton').click(function (e) {
-        e.preventDefault();
-        $.get('/api/Migrate'
-          ).done(function(data) {
-            toolsViewModel.migrationStatus(data);
-        });
-    });
 });
