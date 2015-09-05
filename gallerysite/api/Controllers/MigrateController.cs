0c60e95694a766627582f267b207810f320e75d5
Jake <jake@jakebladt.com>
2015-09-04 20:18:31 -0400

    get migration status

--- gallerysite/api/Controllers/MigrateController.cs
+++ gallerysite/api/Controllers/MigrateController.cs
@@ -5,10 +5,15 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using gallerysite.api.ViewModels;
+
 namespace gallerysite.api.Controllers
 {
     public class MigrateController : ApiController
     {
-
+        public MigrationStatusViewModel Get()
+        {
+            return new MigrationStatusViewModel();
+        }
     }
 }
