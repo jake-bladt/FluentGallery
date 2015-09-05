d44cb32be383ed63293f0717e20d2f85d5a4639f
Jake <jake@jakebladt.com>
2015-09-05 00:00:22 -0400

    create db on startup.

--- gallerysite/Global.asax.cs
+++ gallerysite/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,8 @@ using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
 
+using JakeBladt.GalleryTools.DAC;
+
 namespace gallerysite
 {
     public class Global : HttpApplication
@@ -15,9 +18,17 @@ namespace gallerysite
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            CreateDB();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+
+        public void CreateDB()
+        {
+            var cnStr = ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString;
+            var manager = new DbManager(cnStr);
+            manager.CreateDatabase();
         }
     }
 }
