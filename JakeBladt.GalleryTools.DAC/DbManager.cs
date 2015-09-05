60e583b086020339ffb385572e983a129a9dab6a
Jake <jake@jakebladt.com>
2015-09-04 23:57:25 -0400

    create database

--- JakeBladt.GalleryTools.DAC/DbManager.cs
+++ JakeBladt.GalleryTools.DAC/DbManager.cs
@@ -4,22 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using NHibernate;
 using FluentNHibernate;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
 
 namespace JakeBladt.GalleryTools.DAC
 {
-    public static class DbManager
+    public class DbManager
     {
-        public static ISessionFactory CreateSessionFactory(string cnStr)
+        protected string _cnStr;
+
+        public DbManager(string cnStr)
+        {
+            _cnStr = cnStr;
+        }
+
+        public ISessionFactory CreateSessionFactory()
+        {
+            return GetDBConfig().BuildSessionFactory();
+        }
+
+        public FluentConfiguration GetDBConfig()
         {
             return Fluently.Configure()
                 .Database(MsSqlConfiguration
-                .MsSql2012.ConnectionString(cnStr))
-                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Subject>())
-                .BuildSessionFactory();
+                .MsSql2012.ConnectionString(_cnStr))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Subject>());
         }
+
+        public void CreateDatabase()
+        {
+            GetDBConfig().ExposeConfiguration(c => new SchemaUpdate(c).Execute(true, true)).BuildSessionFactory();
+        }
+
     }
 }
