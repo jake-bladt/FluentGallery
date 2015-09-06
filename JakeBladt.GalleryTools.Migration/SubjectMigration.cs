fbe82303a11ff71e446c358ee0a68de33f2bfcf6
Jake <jake@jakebladt.com>
2015-09-06 11:24:18 -0400

    subject migration

---
+++ JakeBladt.GalleryTools.Migration/SubjectMigration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JakeBladt.GalleryTools.Repositories;
+
+namespace JakeBladt.GalleryTools.Migration
+{
+    public class SubjectMigration
+    {
+        protected FsSubjectRepository _FileSystemRepo;
+        protected DbSubjectRepository _DatabaseRepo;
+
+        public SubjectMigration(FsSubjectRepository fs, DbSubjectRepository db)
+        {
+
+        }
+    }
+}
