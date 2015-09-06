9ba8e92d862eb4466651963a62dc6044fc4e3b8e
Jake <jake@jakebladt.com>
2015-09-06 11:48:30 -0400

    file prefix to display name

--- JakeBladt.GalleryTools.DAC/Subject.cs
+++ JakeBladt.GalleryTools.DAC/Subject.cs
@@ -16,6 +16,25 @@ namespace JakeBladt.GalleryTools.DAC
         public virtual string DisplayName { get; set; }
         public virtual int FileCount { get; set; }
         public virtual DateTime AddedAt { get; set; }
+
+        public static string FilePrefixToName(string filePrefix)
+        {
+            var chars = filePrefix.Replace('.', ' ').ToCharArray();
+            StringBuilder retBuilder = new StringBuilder(chars[0].ToString().ToUpper());
+            for(int i = 1; i < chars.Length; i++)
+            {
+                if(chars[i -1] == ' ')
+                {
+                    retBuilder.Append(chars[i].ToString().ToUpper());
+                }
+                else
+                {
+                    retBuilder.Append(chars[i]);
+                }
+            }
+            if (chars[chars.Length - 1] == ' ') retBuilder.Append('.');
+            return retBuilder.ToString();
+        }
     }
 
     public class SubjectMap : ClassMap<Subject>
